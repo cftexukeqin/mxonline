576482ae943d6c7ca947bc3d6864993a67c3940e
xukeqin <wwlzxukeqing@163.com>
2019-03-04 20:40:33 +0800

    modify regist bug

--- apps/utils/email_send.py
+++ apps/utils/email_send.py
@@ -38,7 +38,7 @@ def send_regist_email(email,code_num=16,send_type='register'):
     #　注册发送邮件内容
     if send_type == "register":
         email_title = "优课课堂邮箱验证"
-        email_body = "请点击下面的链接激活你的账号: http://127.0.0.1:8000/user/active/{0}/".format(code)
+        email_body = "请点击下面的链接激活你的账号: http://118.126.108.129/user/active/{0}/".format(code)
 
         #　使用Django内置函数完成邮件发送
         send_status = send_mail(email_title,email_body,EMAIL_FROM,[email])
@@ -49,7 +49,7 @@ def send_regist_email(email,code_num=16,send_type='register'):
 
     if send_type == "forget":
         email_title = "优课课堂邮箱验证"
-        email_body = "请点击下面的链接重置你的账号: http://127.0.0.1:8000/user/reset/{0}/".format(code)
+        email_body = "请点击下面的链接重置你的账号: http://118.126.108.129//user/reset/{0}/".format(code)
 
         #　使用Django内置函数完成邮件发送
         send_status = send_mail(email_title,email_body,EMAIL_FROM,[email])
